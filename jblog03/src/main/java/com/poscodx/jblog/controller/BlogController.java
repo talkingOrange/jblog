762fdad3dee1bbe8048cb31ea54867bd52bc6a8c
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-06 09:06:36 +0900

    [ feat ] authUser와 blogId 비교로 url 유입 막기

--- jblog03/src/main/java/com/poscodx/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.poscodx.jblog.security.AuthUser;
 import com.poscodx.jblog.service.BlogService;
 import com.poscodx.jblog.service.CategoryService;
 import com.poscodx.jblog.service.PostService;
@@ -20,6 +23,7 @@ import com.poscodx.jblog.service.fileUploadService;
 import com.poscodx.jblog.vo.BlogVo;
 import com.poscodx.jblog.vo.CategoryVo;
 import com.poscodx.jblog.vo.PostVo;
+import com.poscodx.jblog.vo.UserVo;
 
 @Controller
 @RequestMapping("/{id}")
@@ -36,45 +40,53 @@ public class BlogController {
 
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private PostService postService;
 
 	@RequestMapping({ "" })
-	public String index(@PathVariable("id") String blogId, Model model, @RequestParam(value="c", required=true, defaultValue="") Long categoryNo, @RequestParam(value="n", required=true, defaultValue="") Long postNo) {
+	public String index(@PathVariable("id") String blogId, Model model,
+			@RequestParam(value = "c", required = true, defaultValue = "") Long categoryNo,
+			@RequestParam(value = "n", required = true, defaultValue = "") Long postNo) {
 		BlogVo blogVo = blogService.getBlog(blogId);
-		
+
 		servletContext.setAttribute("blogTitle", blogVo.getTitle()); // "blogTitle"로 저장
 		servletContext.setAttribute("blogId", blogVo.getBlog_id()); // "blogId"로 저장
 
 		// category값
 		List<CategoryVo> list = categoryService.getCategory(blogId);
 		model.addAttribute("list", list);
-		
-		
+
 		// post값
 		List<PostVo> postList = postService.getPost(blogId, categoryNo);
 		model.addAttribute("postList", postList);
-		
+
 		// post 개별 값
-		PostVo postVo = postService.getPostDetail(blogId,categoryNo, postNo);
-		
+		PostVo postVo = postService.getPostDetail(blogId, categoryNo, postNo);
+
 		model.addAttribute("categoryNo", categoryNo);
 		model.addAttribute("postNo", postNo);
-		
+
 		model.addAttribute("postVo", postVo);
 
 		model.addAttribute("blogVo", blogVo);
 		return "blog/main";
 	}
-	
-	
 
 	// 블로그 기본 관리
 
 	@RequestMapping("/admin/basic")
-	public String adminBasic(@PathVariable("id") String blogId, Model model) {
+	public String adminBasic(@PathVariable("id") String blogId, HttpServletRequest request, Model model) {
 		BlogVo blogVo = blogService.getBlog(blogId);
+		HttpSession session = request.getSession();
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
+
+		if (!authUser.getId().equals(blogId)) {
+			
+			return "redirect:/{id}";
+		}
+		System.out.println(authUser.getId());
+		System.out.println(blogId);
 
 		model.addAttribute("selectedPage", "basic");
 		model.addAttribute("blogVo", blogVo);
@@ -119,7 +131,7 @@ public class BlogController {
 
 	@RequestMapping("/admin/category/delete/{no}")
 	public String delete(@PathVariable("no") Long no, @PathVariable("id") String blogId, CategoryVo vo) {
-		System.out.println("카테고리에 대한 정보"+vo);
+		System.out.println("카테고리에 대한 정보" + vo);
 		categoryService.deleteCategory(no, blogId);
 		postService.deletePost(no);
 
@@ -135,16 +147,17 @@ public class BlogController {
 		// category값
 		List<CategoryVo> list = categoryService.getCategory(blogId);
 		model.addAttribute("list", list);
-		
+
 		model.addAttribute("selectedPage", "write");
 		return "blog/admin-write";
 	}
-	
+
 	@RequestMapping("/admin/write/add")
-	public String addWrite(@PathVariable("id") String blogId, PostVo postVo, @RequestParam(value="c", required=true, defaultValue="1") Long category) {
+	public String addWrite(@PathVariable("id") String blogId, PostVo postVo,
+			@RequestParam(value = "c", required = true, defaultValue = "1") Long category) {
 		postVo.setCategory_no(category);
 		postService.addPost(postVo);
-		
+
 		categoryService.updateCategoryPostNum(category);
 
 		return "redirect:/{id}";
