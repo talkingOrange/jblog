3e51f715bbb773865ca8dbc795bc93c935e56e85
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-10 20:49:08 +0900

    [ feat ] Component-scan xml->java

--- jblog04/src/main/java/com/poscodx/jblog/config/AppConfig.java
+++ jblog04/src/main/java/com/poscodx/jblog/config/AppConfig.java
@@ -1,5 +1,12 @@
 package com.poscodx.jblog.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan({"com.poscodx.jblog.repository", "com.poscodx.jblog.service"})
 public class AppConfig {
 
 }
