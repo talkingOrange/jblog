83ba587088ab34c28b5ef37f93bb2c26be0e955e
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-10 21:15:27 +0900

    [ feat ] jdbc env

--- jblog04/src/main/java/com/poscodx/jblog/config/DBConfig.java
+++ jblog04/src/main/java/com/poscodx/jblog/config/DBConfig.java
@@ -3,21 +3,26 @@ package com.poscodx.jblog.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:com/poscodx/jblog/config/app/jdbc.properties")
 public class DBConfig {
-
+	@Autowired
+	private Environment env;
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		dataSource.setUrl("jdbc:mariadb://192.168.0.178:3307/jblog?charset=utf8");
-		dataSource.setUsername("jblog");
-		dataSource.setPassword("jblog");
-		dataSource.setInitialSize(10);
-		dataSource.setMaxActive(20);
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		dataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
+		dataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
 		
 		return dataSource;
 	}
